b1311f6ebf7ced2973f331d22d9c0e59cf75b910
Zxc377 <bfruits223@gmail.com>
2025-11-26 13:11:18 +0600

    finish

---
+++ movie_app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(UserProfile)
+admin.site.register(Country)
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieLanguages)
+admin.site.register(Moments)
+admin.site.register(Rating)
+admin.site.register(Favorite)
+admin.site.register(FavoriteMovie)
+admin.site.register(History)
