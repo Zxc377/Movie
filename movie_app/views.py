852963beb35bd09a8d15741b7b118320f12b14d5
Zxc377 <bfruits223@gmail.com>
2025-12-08 13:17:11 +0600

    i have finish

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import (UserProfile, Country, Director, Actor, Genre, Movie,
+                     MovieLanguages,Moments,Rating, Favorite, FavoriteMovie, History)
 
-# Create your views here.
+from .serializers import (UserSerializer, CountrySerializer, CountryDetailSerializer,
+                          DirectorSerializer, DirectorDetailSerializer, ActorSerializer,
+                         GenreSerializer, MovieListSerializer, MovieDetailSerializer,
+                         MovieLanguagesSerializer, MomentsSerializer, RatingSerializer,
+                         FavoriteSerializer, FavoriteMovieSerializer, HistorySerializer, ActorDetailSerializer, GenreDetailSerializer)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return UserProfile.objects.filter(id=self.request.user.id)
+
+class CountryListAPIView(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CountryDetailAPIView(generics.RetrieveAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountryDetailSerializer
+
+
+class DirectorListAPIView(generics.ListAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+
+class DirectorDetailAPIView(generics.RetrieveAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorDetailSerializer
+
+class ActorListAPIView(generics.ListAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class ActorDetailAPIView(generics.RetrieveAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializer
+
+
+class GenreListAPIView(generics.ListAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreDetailAPIView(generics.RetrieveAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreDetailSerializer
+
+
+class MovieListAPIView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieListSerializer
+
+
+class MovieDetailAPIView(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieDetailSerializer
+
+
+class MovieLanguagesViewSet(viewsets.ModelViewSet):
+    queryset = MovieLanguages.objects.all()
+    serializer_class = MovieLanguagesSerializer
+
+
+class MomentsViewSet(viewsets.ModelViewSet):
+    queryset = Moments.objects.all()
+    serializer_class = MomentsSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+
+
+class FavoriteMovieViewSet(viewsets.ModelViewSet):
+    queryset = FavoriteMovie.objects.all()
+    serializer_class = FavoriteMovieSerializer
+
+
+class HistoryViewSet(viewsets.ModelViewSet):
+    queryset = History.objects.all()
+    serializer_class = HistorySerializer
